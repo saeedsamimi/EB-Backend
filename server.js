2655ccc13134eb1ce17065b13f6726ead119d74b
saeedsamimi <doob.fooy@gmail.com>
2024-02-06 19:36:37 +0330

    few changes

--- server.js
+++ server.js
@@ -6,6 +6,28 @@ const express = require("express"),
 require("./database/connection");
 require("dotenv").config();
 
+function generateAccessToken(username) {
+  return jwt.sign(username, process.env.SECURE_TOKEN, { expiresIn: "1800s" });
+}
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    // taking log from the error
+    console.log(err);
+    // set the error state of the result
+    if (err) return res.sendStatus(403);
+    // init user real value
+    req.user = user;
+    // then go to the next middleware EXPRESS.js function
+    next();
+  });
+}
+
 app.use(express.json());
 app.use(cors());
 
@@ -16,16 +38,19 @@ app.post("/signin", (req, res) => {
       userModel
         .insertMany(req.body)
         .then(() => {
-          res.json({ msg: "You are signed in!" });
+          const jwt_token = generateAccessToken(req.body.username);
+          res.json({ result: true, token: jwt_token });
         })
         .catch((error) => {
           if (error.code === 11000) {
             res.status(500).json({
+              result: false,
               msg: "cannot SignIn!",
               reason: "the username or email already exists!",
             });
           } else {
             res.status(500).json({
+              result: false,
               msg: "An error occured",
               reason: "an unexpected error occured!",
             });
@@ -35,21 +60,21 @@ app.post("/signin", (req, res) => {
   });
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", authenticateToken, (req, res) => {
   const findUser = { username: req.body.username, email: req.body.email };
   userModel
     .findOne(findUser)
     .then(function ({ password }) {
       bcrypt.compare(req.body.password, password, function (err, result) {
         if (result) {
-          res.json({ msg: "you are signed in!" });
+          res.json({ result: true });
         } else {
-          res.json({ msg: "you password not correct!" });
+          res.json({ result: false, msg: "your password not correct!" });
         }
       });
     })
     .catch((error) => {
-      res.status(500).json({ msg: "the user not exist!" });
+      res.status(500).json({ result: false, msg: "the user not exist!" });
     });
 });
 
