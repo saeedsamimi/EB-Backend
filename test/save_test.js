ba5f87cc622f102d4f452a3e6144c2dd2d7f19c7
saeedsamimi <doob.fooy@gmail.com>
2024-02-06 19:23:28 +0330

    few changes

--- test/save_test.js
+++ test/save_test.js
@@ -2,6 +2,8 @@ const mocha = require("mocha");
 const assert = require("assert");
 const userModel = require("../models/users");
 const tempUser = require("./test.config");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 describe("Test saving the data", function (done) {
   it("save the first one", function () {
@@ -15,5 +17,12 @@ describe("Test saving the data", function (done) {
         assert(model.isNew === true, error.code || error.message);
         done();
       });
+  });
+
+  it("generate the web token using JWT", function () {
+    crypto.randomBytes(32, (err, buf) => {
+      if (err) throw err;
+      console.log("the token generated: " + buf.toString("base64"));
+    });
   });
 });
