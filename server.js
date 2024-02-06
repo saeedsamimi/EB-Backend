d93f4b88b403418e0bea580fc8db64c97ff5acc1
saeedsamimi <doob.fooy@gmail.com>
2024-02-06 18:59:07 +0330

    few changes

--- server.js
+++ server.js
@@ -1,32 +1,55 @@
-const express = require("express");
-const app = express();
-const userModel = require("./models/users");
-const cors = require("cors");
+const express = require("express"),
+  app = express(),
+  userModel = require("./models/users"),
+  cors = require("cors"),
+  bcrypt = require("bcrypt");
 require("./database/connection");
 require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/signin", (req, res) => {
+app.post("/signin", (req, res) => {
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(req.body.password, salt, function (err, hash) {
+      req.body.password = hash;
+      userModel
+        .insertMany(req.body)
+        .then(() => {
+          res.json({ msg: "You are signed in!" });
+        })
+        .catch((error) => {
+          if (error.code === 11000) {
+            res.status(500).json({
+              msg: "cannot SignIn!",
+              reason: "the username or email already exists!",
+            });
+          } else {
+            res.status(500).json({
+              msg: "An error occured",
+              reason: "an unexpected error occured!",
+            });
+          }
+        });
+    });
+  });
+});
+
+app.post("/login", (req, res) => {
+  const findUser = { username: req.body.username, email: req.body.email };
   userModel
-    .insertMany(req.body)
-    .then(() => {
-      res.json({ msg: "You are signed in!" });
+    .findOne(findUser)
+    .then(function ({ password }) {
+      bcrypt.compare(req.body.password, password, function (err, result) {
+        if (result) {
+          res.json({ msg: "you are signed in!" });
+        } else {
+          res.json({ msg: "you password not correct!" });
+        }
+      });
     })
     .catch((error) => {
-      if (error.code === 11000) {
-        res.json({
-          msg: "cannot SignIn!",
-          reason: "the username or email already exists!",
-        });
-      } else {
-        res.json({
-          msg: "An error occured",
-          reason: "an unexpected error occured!",
-        });
-      }
-      res.status(500);
+      res.status(500).json({ msg: "the user not exist!" });
     });
 });
 
